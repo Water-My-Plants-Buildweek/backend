3179b6e962b043729d05d415a045367fa7617368
Andrew Sitzes <60900968+Drewcifer88@users.noreply.github.com>
2020-07-27 16:30:35 -0700

    plant

--- plants/plantModel.js
+++ plants/plantModel.js
@@ -1,21 +1,21 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
-    findById,
-    add,
-    update,
-    remove,
+  findById,
+  add,
+  update,
+  remove,
 };
 function findById(id) {
-    return db("plants").where({ id }).first();
+  return db("plants").where({ id }).first();
 }
 
 function add(plant) {
-    return db("plants").insert(plant);
+  return db("plants").insert(plant);
 }
 function update(change, id) {
-    return db("posts").where({ id }).update(change);
+  return db("plants").where({ id }).update(change);
 }
 function remove(id) {
-    return db("plants").where({ id }).del();
+  return db("plants").where({ id }).del();
 }
