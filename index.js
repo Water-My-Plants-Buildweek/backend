62fa598746cb4e5201618ed1ca650478baba5716
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2020-07-27 12:52:05 -0400

    file structured

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+const server = require("./api/server");
+const port = process.env.PORT || 5010;
+server.listen(port, () => console.log(`== Server running on port ${port} ==`));
---
+++ knexfile.js
@@ -0,0 +1,56 @@
+// Update with your config settings.
+const pgConnection =
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/wunderlist";
+module.exports = {
+    development: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./database/wunderlist.db3",
+        },
+        migrations: {
+            directory: "./database/migrations",
+        },
+        seeds: {
+            directory: "./database/seeds",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
+    },
+    testing: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./database/test.db3",
+        },
+        migrations: {
+            directory: "./database/migrations",
+        },
+        seeds: {
+            directory: "./database/seeds",
+        },
+        pool: {
+            afterCreate: (conn, done) => {
+                conn.run("PRAGMA foreign_keys = ON", done);
+            },
+        },
+    },
+
+    production: {
+        client: "pg",
+        connection: pgConnection,
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            directory: "./database/migrations",
+        },
+        seeds: {
+            directory: "./database/seeds/production",
+        },
+    },
+};
