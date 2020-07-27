63d7d05d1592050826b34f8bfca81e0d7eed43e8
Andrew Sitzes <60900968+Drewcifer88@users.noreply.github.com>
2020-07-27 10:43:46 -0700

    migrations

--- database/migrations/20200727130027_init.js
+++ database/migrations/20200727130027_init.js
@@ -1,8 +1,26 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (users) => {
+      users.increments();
 
-exports.up = function(knex) {
-  
+      users.string("username", 255).notNullable().unique();
+
+      users.string("password", 255).notNullable();
+
+      users.string("phone-number", 10).unique();
+    })
+
+    .createTable("plants", (plants) => {
+      plants.increments();
+
+      plants.string("nickname", 255).notNullable();
+
+      plants.string("species", 255).notNullable();
+
+      plants.integer("water-frequency", 10).notNullable();
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("plants");
 };
