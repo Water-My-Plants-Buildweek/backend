3776b6fda8b81bf499a61eaa4c5a434c73e224a2
Andrew Sitzes <60900968+Drewcifer88@users.noreply.github.com>
2020-07-28 10:26:10 -0700

    phone water

--- database/migrations/20200727130027_init.js
+++ database/migrations/20200727130027_init.js
@@ -1,20 +1,20 @@
 exports.up = function (knex) {
-    return knex.schema
-        .createTable("users", (users) => {
-            users.increments();
-            users.string("username", 255).notNullable().unique();
-            users.string("password", 255).notNullable();
-            users.string("phone-number", 10).unique();
-        })
+  return knex.schema
+    .createTable("users", (users) => {
+      users.increments();
+      users.string("username", 255).notNullable().unique();
+      users.string("password", 255).notNullable();
+      users.string("phone", 10).unique();
+    })
 
-        .createTable("plants", (plants) => {
-            plants.increments();
-            plants.string("nickname", 255).notNullable();
-            plants.string("species", 255).notNullable();
-            plants.integer("water-frequency", 10).notNullable();
-        });
+    .createTable("plants", (plants) => {
+      plants.increments();
+      plants.string("nickname", 255).notNullable();
+      plants.string("species", 255).notNullable();
+      plants.integer("h2ofrequency", 10).notNullable();
+    });
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists("users").dropTableIfExists("plants");
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("plants");
 };
