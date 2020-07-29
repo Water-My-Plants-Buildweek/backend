e49640559c166439422dbb43a046f6759103a1ea
Andrew Sitzes <60900968+Drewcifer88@users.noreply.github.com>
2020-07-29 11:46:09 -0700

    edit user info

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,4 +1,4 @@
 const knex = require("knex");
 const environment = process.env.DB_ENV || "development";
-const config = require("../knexfile");
-module.exports = knex(config[environment]);
+const knexfile = require("../knexfile");
+module.exports = knex(knexfile[environment]);
