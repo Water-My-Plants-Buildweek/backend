9b11467175ee163251f32a46830b7cb6d36bd4c6
Andrew Sitzes <60900968+Drewcifer88@users.noreply.github.com>
2020-07-29 12:19:00 -0700

    editing

--- auth/usersModel.js
+++ auth/usersModel.js
@@ -1,36 +1,38 @@
 const db = require("../database/dbConfig");
 
 module.exports = {
-    add,
-    find,
-    findBy,
-    findById,
-    remove,
+  add,
+  find,
+  findBy,
+  findById,
+  remove,
 };
 
 function find() {
-    return db("users as u").where("u.username").orderBy("u.id");
+  return db("users as u").where("u.username").orderBy("u.id");
 }
 
 function findBy(filter) {
-    console.log("filter", filter);
-    return db("users as u").where(filter);
+  console.log("filter", filter);
+  return db("users as u").where(filter);
 }
 
 async function add(users) {
-    try {
-        const [id] = await db("users").insert(users, "id");
+  try {
+    const [id] = await db("users").insert(users, "id");
 
-        return findById(id);
-    } catch (error) {
-        throw error;
-    }
+    return findById(id);
+  } catch (error) {
+    throw error;
+  }
 }
 
 function findById(id) {
-    return db("users").where({ id }).first();
+  return db("users").where({ id }).first();
 }
 
 function remove(id) {
-    return db("users").where({ id }).del();
+  return db("users").where({ id }).del();
 }
+
+//testing
